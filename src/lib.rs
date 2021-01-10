c0ae6f3368db21d01b687cfe203cad01d0d3eeb8
monksc <ccmonks32@gmail.com>
2021-01-10 08:57:16 -0500

    Made Terminal pub

--- src/lib.rs
+++ src/lib.rs
@@ -3,5 +3,6 @@ pub mod reading_lang;
 pub use reading_lang::Interpreter;
 pub use reading_lang::LanguageProduction;
 pub use reading_lang::LanguageProductionOrTerm;
+pub use reading_lang::Terminal;
 
 
